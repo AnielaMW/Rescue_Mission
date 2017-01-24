e31a9a7b4648025e3f0c00c67dc39fb6a643daed
Aniela M Wolkonowski <anielamw@gmail.com>
2017-01-24 17:33:00 -0500

    Plan of action

---
+++ rescue_mission/spec/features/1_user_views_posts.rb
@@ -0,0 +1,13 @@
+require "spec_helper"
+
+feature "User views list of posts" do
+  # As a user
+  # I want to view recently posted questions
+  # So that I can help others
+
+  # Acceptance Criteria
+  # - I must see the title of each question
+  # - I must see questions listed in order, most recently posted first
+
+  pending "successfully view alphabetized list of meetups"
+end
